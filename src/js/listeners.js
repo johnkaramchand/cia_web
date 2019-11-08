44122d7425d0802fafc01b794f7631cb9bc92006
Raj <raj.fps2000@gmail.com>
2019-11-09 00:11:23 +0530

    text design 2

---
+++ src/js/listeners.js
@@ -0,0 +1,14 @@
+let navbtns = [
+    document.querySelector('.home'),
+    document.querySelector('.about'),
+    document.querySelector('.board'),
+    document.querySelector('.stories'),
+    document.querySelector('.contact')
+];
+
+navbtns.forEach((btn) => {
+
+    btn.addEventListener('click', () => {
+        
+    });
+});
