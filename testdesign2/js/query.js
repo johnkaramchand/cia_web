acdf507a259fea1078f7f1bf7d0d1428708447f9
Raj <raj.fps2000@gmail.com>
2019-11-12 16:32:48 +0530

    move

--- testdesign2/js/query.js
+++ testdesign2/js/query.js
@@ -1,6 +1,12 @@
+const call = () => {
+    fetch('https://cia.atria.edu/api/v2/events/?format=json', {
+        method: 'get'
+    }).then(response => response.json()).then(jsonData => console.log(jsonData));
+};
 const makeEvent = () => {
-    
+
 };
 const getEvents = () => {
 
 };
+call()
