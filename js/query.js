929bf4f11f27a5c4b277cc3b64394de6cbf69b38
Raj <raj.fps2000@gmail.com>
2020-01-12 20:59:26 +0530

    events

--- js/query.js
+++ js/query.js
@@ -1,12 +1,13 @@
 const call = () => {
     fetch('https://cia.atria.edu/api/v2/events/?format=json', {
         method: 'get'
-    }).then(response => response.json()).then(jsonData => console.log(jsonData));
+    }).then(response => response.json())
+        .then(jsonData => console.log(jsonData[0]));
 };
 const makeEvent = () => {
-
+    
 };
 const getEvents = () => {
 
 };
-call()
+//call()
--- testdesign2/setup.js
+++ testdesign2/setup.js
@@ -1,23 +1,6 @@
-var scripts = [
-    'gsap/css.js',
-    'gsap/tlm.js',
-    'gsap/tm.js',
-    'intro.js',
-    'navbar.js',
-    'query.js',
-    'main.js'
-];
 var styles = [
     'main.css'
 ];
-let scriptSetup = (scripts) => {
-    scripts.forEach(script => {
-        const scr = document.createElement('script');
-        scr.src = './js/' + script;
-        console.log(scr);
-        document.body.appendChild(scr);
-    });
-}
 let styleSetup = (styles) => {
     styles.forEach(style => {
         const link = document.createElement('link');
