762068cbb20e225a831fa2b2d5f55d3b0c4dfc6b
Raj <raj.fps2000@gmail.com>
2020-01-12 21:42:33 +0530

    createEvent function

--- js/intro.js
+++ js/intro.js
@@ -4,5 +4,10 @@ function intro() {
         .to('.cover-screen', 1, { height: '0', ease: Expo.easeOut })
         .to('.intro-text h1', 1, { opacity: '0', ease: Expo.easeOut }, '-=1')
         .to('.intro-text h1', 0, { fontSize: '0em' });
+
+    const colors = ['rgb(222, 228, 226)', 'white', 'rgb(218, 218, 218)','rgb(240, 253, 255)']
+    setInterval(()=>{
+        document.body.style.setProperty('--bgtheme', colors[Math.floor(Math.random() * colors.length)])
+    },2000)
 }
 intro();
--- js/query.js
+++ js/query.js
@@ -1,3 +1,11 @@
+function eventCreate(title,discription,date,img){
+    let event=document.createElement('div');
+    let parent=document.querySelector('.events-section');
+    event.setAttribute('class','event');
+    event.innerHTML = '<div class="title-discription"><h1 class="event-title" >'+title+'</h1><p class="event-discription">'+discription+'</p></div><img src="'+img+'" alt=""><div class="event-date"><h1>'+date+'</h1></div>'
+    parent.appendChild(event);
+}
+
 const call = () => {
     fetch('https://cia.atria.edu/api/v2/events/?format=json', {
         method: 'get'
@@ -11,3 +19,7 @@ const getEvents = () => {
 
 };
 //call()
+
+for(let i=0;i<2;i++){
+    eventCreate('Test title', 'test discription', '1st Jan', 'https://blog.printsome.com/wp-content/uploads/big-events-vs-small-events.jpg');
+}
