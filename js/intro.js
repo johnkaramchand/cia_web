56dd97247be1f2b1ffc4cf77a95625eba90b8037
Raj <raj.fps2000@gmail.com>
2020-01-18 14:56:01 +0530

    event api enabled

--- js/intro.js
+++ js/intro.js
@@ -4,7 +4,7 @@ function intro() {
     t.to('.intro-text h1', 1, { letterSpacing: '0px', ease: Expo.easeOut })
         .to('.cover-screen', 1, { height: '0', ease: Expo.easeOut })
         .to('.intro-text h1', 1, { opacity: '0', ease: Expo.easeOut }, '-=1')
-        .to('.intro-text h1', 0, { fontSize: '0em' });
+        .to('.intro-text h1', 1, { fontSize: '0em' }, '-=1');
 
     /* const colors = ['rgb(212, 255, 242)', 'white', 'rgb(221, 240, 234)', 'rgb(226, 252, 255)']
     if (!mobileFlag) setInterval(() => {
