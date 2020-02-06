4544decddae836c1903597cfc7c0ab78f78d6fe5
Raj <raj.fps2000@gmail.com>
2020-02-06 13:13:11 +0530

    switchded to tabs UI for schedule

--- hackathon/js/navigation.js
+++ hackathon/js/navigation.js
@@ -18,6 +18,7 @@
         }
     }
     observer.observe(landing);
+
     let sections = [
         document.querySelector('.about'),
         document.querySelector('.cia19'),
@@ -34,5 +35,38 @@
             sections[i].scrollIntoView();
         })
     })
+
+    const tabs = document.querySelectorAll('.tabs>h2');
+    switchTo(0);
+    function switchTo(tabindex) {
+        tabs[tabindex].setAttribute('class', 'selected-tab');
+        tabs[Number(!tabindex)].setAttribute('class', '');
+
+        const sections = document.querySelectorAll('.day-section');
+        animationTimeline.fromTo(sections[Number(!tabindex)], .1, { opacity: '1' }, {
+            opacity: '0', onComplete: () => {
+
+                sections[tabindex].style.display = 'block';
+                sections[Number(!tabindex)].style.display = 'none';
+            }
+
+        })
+        animationTimeline.fromTo(sections[tabindex], .1, { opacity: '0' }, {
+            opacity: '1', onComplete: () => {
+
+                sections[Number(!tabindex)].style.display = 'none';
+
+            }
+        })
+
+
+    }
+    tabs.forEach((tab, i) => {
+        tab.addEventListener('click', () => {
+            switchTo(i);
+        })
+    });
+
+
 
 })();
